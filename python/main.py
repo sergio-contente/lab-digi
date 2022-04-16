65054213f78a9e7d51f95f7decc95b26626bb479
Jonas <jonasgomesdemorais@gmail.com>
2022-04-15 23:26:46 -0300

    python script error

--- python/main.py
+++ python/main.py
@@ -1,5 +1,4 @@
 import paho.mqtt.client as mqtt
-import numpy as np
 import time
 import datetime
 
@@ -16,36 +15,48 @@ KeepAlive = 60
 
 tem_letra  = "1"
 tem_jogada = "0"
+aguardando_publish = False
+word_published = False
+first_time = True
+iterador_palavra = 0
 
 palavra = ""
 leds = ["0", "0"]
 
-resultados = "0000000000"
+resultados = "" # <---
 
 jogada_correta = "0"
 
 contagem_letras = 0
 # Quando conectar na rede (Callback de conexao)
 def on_connect(client, userdata, flags, rc):
+  global resultados
+  global contagem_letras
+
   print("Conectado com codigo " + str(rc))
   client.subscribe(user+"/E1", qos=0)
   for i in range(0, 5):
     client.subscribe(user+"/S" + str(i), qos=0)
   for i in range(0,8):
     client.publish(user+"/E" + str(i), payload = "0", qos=0)
+  resultados = ""
+  contagem_letras = 0
 
-def publish_word(palavra):
+def publish_letter(letter):
   print(palavra)
   global palavra_ant
   global enable
   global tem_letra
-  
+  global iterador_palavra
   
   enable = 1
   palavra_ant = palavra
-  for letra in palavra:
-    client.publish(user+"/RX", payload=letra, qos=0)
-    time.sleep(2)
+  # for letra in palavra:
+  #   if aguardando_publish == False:
+  client.publish(user+"/RX", payload=letter, qos=0)
+  time.sleep(2)
+  iterador_palavra += 1
+
     # print(enable)
     # letra_bin = get_signal(letra)
     # enable_bin = np.binary_repr(enable,3)
@@ -65,19 +76,27 @@ def publish_word(palavra):
 
 def pronto_publish():
   global contagem_letras
+  global resultados
   global tempo
   global contagem
   global leds
-
-  if contagem == 2 or datetime.datetime.now().second - tempo > 1:
+  global aguardando_publish
+  #print(f'datetime.datetime.now().second - tempo: {datetime.datetime.now().second - tempo}')
+  if contagem == 2 or datetime.datetime.now().second - tempo > 5:
+    print("\n>>>", datetime.datetime.now().second - tempo, "\n")
     print("Recebi uma cor")
     client.publish(user+"/E" + "3", payload = "1", qos=0)
     time.sleep(1)
     client.publish(user+"/E" + "3", payload = "0", qos=0)
+    time.sleep(1)
+    print(f'contagem : {contagem}')
     contagem=0
-    resultados.append(leds)
+    resultados += leds[0] + leds[1]
     contagem_letras += 1
-  
+    #tempo = 0
+    aguardando_publish = False
+    print(f'resultados : {resultados}')
+    print(f'contagem_letras : {contagem_letras}')
 
 # Quando receber uma mensagem (Callback de mensagem)
 def on_message(client, userdata, msg):
@@ -85,21 +104,25 @@ def on_message(client, userdata, msg):
   global palavra
   global contagem
   global tempo
-  
+  global aguardando_publish
+  global word_published
+
+  aguardando_publish = True
   tempo = datetime.datetime.now().second
   palavra = str(msg.payload.decode("utf-8"))
   print(str(msg.topic)+" "+str(msg.payload.decode("utf-8")))
 
   if str(msg.topic) == user+"/E1":
     print("Recebi uma mensagem de E1")
+    word_published = False
   elif str(msg.topic) == user+"/S0" or str(msg.topic) == user+"/S1": # SOMAR STRING TODA VEZ QUE 
     if str(msg.topic) == user+"/S0":
       leds[0] = str(msg.payload.decode("utf-8"))
     elif str(msg.topic) == user+"/S1":
-      leds[1] = str(msg.payload.decode("utf-8"))
+      leds[1] = str(msg.payload.decode("utf-8")) 
+    contagem += 1
   else:
     print("Erro! Mensagem recebida de tópico estranho")
-  contagem += 1
 
 contagem=0
 
@@ -117,25 +140,35 @@ client.connect(Broker, Port, KeepAlive)
 
 client.loop_start() 
 # A primeira mensagem costuma ser perdida aqui no notebook
-client.publish(user+"/S0", payload="0", qos=0, retain=False)
+#client.publish(user+"/S0", payload="0", qos=0, retain=False)
 palavra_ant = palavra
 time.sleep(1)
 while True:
-  if palavra_ant != palavra:
-    tem_jogada = "1"
+  #print(f'word_published: {word_published}')
+  if word_published == False: #palavra_ant != palavra or 
+    print(f'palavra {palavra}')
     client.publish(user+"/E0", payload=tem_jogada, qos=0, retain=False)
-    time.sleep(0.00075) #3/4 de ciclo de clock
-    publish_word(palavra)
+    letter = palavra[iterador_palavra]
+    print(f'letter: {letter}')
+    print(f'contagem_letras: {iterador_palavra}')
+    publish_letter(letter)
+    if not first_time:
+      tem_jogada = "1"
+      client.publish(user+"/E0", payload=tem_jogada, qos=0, retain=False)
+      time.sleep(0.00075)
+    else:
+        first_time = False
+        word_published = True
+        iterador_palavra = 0
+  elif tem_jogada == "1" and contagem_letras == 5:
     tem_jogada = "0"
     client.publish(user+"/E0", payload=tem_jogada, qos=0, retain=False)
     time.sleep(0.00075)
-  elif tem_jogada == "1":
-    tem_jogada = "0"
-    client.publish(user+"/E0", payload=tem_jogada, qos=0, retain=False)
-    time.sleep(0.00075)
-  pronto_publish(tempo, contagem, leds)
+  # if aguardando_publish:
+  tempo = datetime.datetime.now().second
+  pronto_publish()
 
-  if contagem_letras == 5:
+  if contagem_letras == 4:
     if resultados == "1111111111":
       jogada_correta = "1"
     else:
@@ -144,7 +177,13 @@ while True:
     time.sleep(1)
     jogada_correta = "0"
     client.publish(user+"/E5", payload=jogada_correta, qos=0, retain=False)
-
+    time.sleep(1)
+    contagem_letras = 0
+    iterador_palavra = 0
+    word_published = True
+    tem_jogada = "0"
+    client.publish(user+"/E0", payload=tem_jogada, qos=0, retain=False)
+    resultados = "" # resetando resultados
 
 client.loop_stop()
 
